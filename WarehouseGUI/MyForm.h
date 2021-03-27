0630d7a2a8d87ecb8ff1a2d0eb7247912908020c
Jacob <jacob.hajjar1@gmail.com>
2021-03-27 15:06:38 -0700

    save

--- WarehouseGUI/MyForm.h
+++ WarehouseGUI/MyForm.h
@@ -35,6 +35,12 @@ namespace WarehouseGUI {
 			}
 		}
 	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox1;
 	protected:
 
 	protected:
@@ -54,14 +60,88 @@ namespace WarehouseGUI {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
-			this->panel1->Location = System::Drawing::Point(131, 64);
+			this->panel1->BackColor = System::Drawing::Color::Transparent;
+			this->panel1->Controls->Add(this->textBox2);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Location = System::Drawing::Point(130, 37);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(450, 300);
+			this->panel1->Size = System::Drawing::Size(450, 361);
 			this->panel1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
+			this->label1->Location = System::Drawing::Point(0, 113);
+			this->label1->MaximumSize = System::Drawing::Size(450, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(450, 46);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Sign In";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->label2->Location = System::Drawing::Point(15, 159);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(86, 20);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Username";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24));
+			this->label3->Location = System::Drawing::Point(0, 0);
+			this->label3->MaximumSize = System::Drawing::Size(450, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(450, 93);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Welcome to the Omni-Warehouse";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(19, 182);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(170, 22);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(19, 242);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(170, 22);
+			this->textBox2->TabIndex = 5;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->label4->Location = System::Drawing::Point(15, 219);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(83, 20);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Password";
+			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
 			// 
 			// MyForm
 			// 
@@ -72,12 +152,22 @@ namespace WarehouseGUI {
 			this->Controls->Add(this->panel1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
 			this->Name = L"MyForm";
-			this->Text = L" ";
+			this->Text = L"Omni-Warehouse";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }

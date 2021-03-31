311866ea36b7b8bb7b9efece5ee16bb6a2265b8c
Jacob <jacob.hajjar1@gmail.com>
2021-03-30 18:23:26 -0700

    save

--- WarehouseGUI/employeeUI.h
+++ WarehouseGUI/employeeUI.h
@@ -34,6 +34,19 @@ namespace WarehouseGUI {
 			}
 		}
 	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -64,15 +77,29 @@ namespace WarehouseGUI {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(employeeUI::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
-			this->panel1->BackColor = System::Drawing::Color::WhiteSmoke;
-			this->panel1->Location = System::Drawing::Point(130, 37);
+			this->panel1->BackColor = System::Drawing::SystemColors::Control;
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Location = System::Drawing::Point(54, 28);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(450, 361);
+			this->panel1->Size = System::Drawing::Size(592, 370);
 			this->panel1->TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(3, 74);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ReadOnly = true;
+			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox1->Size = System::Drawing::Size(586, 189);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = resources->GetString(L"textBox1.Text");
 			// 
 			// employeeUI
 			// 
@@ -84,6 +111,8 @@ namespace WarehouseGUI {
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
 			this->Name = L"employeeUI";
 			this->Text = L"Omni-Warehouse";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
@@ -102,5 +131,8 @@ namespace WarehouseGUI {
 	}
 	private: System::Void textBox2_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+
+	}
+};
 }
